439dcba25a980d54ec20bc663b05b5031328ed7f
Hans-Christoph Steiner <hans@eds.org>
2016-01-04 23:58:50 +0100

    reformat AnimationHelpers to match the rest of the code

--- app/src/main/java/info/guardianproject/securereaderinterface/uiutil/AnimationHelpers.java
+++ app/src/main/java/info/guardianproject/securereaderinterface/uiutil/AnimationHelpers.java
@@ -13,157 +13,125 @@ import android.view.animation.ScaleAnimation;
 import android.view.animation.TranslateAnimation;
 
 @SuppressLint("NewApi")
-public class AnimationHelpers
-{
+public class AnimationHelpers {
 
-	public static void translateY(final View view, float fromY, float toY, long duration)
-	{
-		if (Build.VERSION.SDK_INT >= 12)
-		{
-			if (duration == 0)
-				view.setTranslationY(toY);
-			else
-				view.animate().translationY(toY).setDuration(duration).start();
-		}
-		else
-		{
-			TranslateAnimation translate = new TranslateAnimation(0, 0, fromY, toY);
-			translate.setDuration(duration);
-			translate.setFillEnabled(true);
-			translate.setFillBefore(true);
-			translate.setFillAfter(true);
-			addAnimation(view, translate);
-		}
-	}
+    public static void translateY(final View view, float fromY, float toY, long duration) {
+        if (Build.VERSION.SDK_INT >= 12) {
+            if (duration == 0)
+                view.setTranslationY(toY);
+            else
+                view.animate().translationY(toY).setDuration(duration).start();
+        } else {
+            TranslateAnimation translate = new TranslateAnimation(0, 0, fromY, toY);
+            translate.setDuration(duration);
+            translate.setFillEnabled(true);
+            translate.setFillBefore(true);
+            translate.setFillAfter(true);
+            addAnimation(view, translate);
+        }
+    }
 
-	public static void scale(final View view, float fromScale, float toScale, long duration, final Runnable whenDone)
-	{
-		if (Build.VERSION.SDK_INT >= 12)
-		{
-			if (duration == 0)
-			{
-				view.setScaleX(toScale);
-				view.setScaleY(toScale);
-				if (whenDone != null)
-					whenDone.run();
-			}
-			else
-			{
-				ViewPropertyAnimator animation = view.animate().scaleX(toScale).scaleY(toScale).setDuration(duration);
-				if (whenDone != null)
-				{
-					animation.setListener(new AnimatorListener()
-					{
-						@Override
-						public void onAnimationCancel(Animator animation)
-						{
-							whenDone.run();
-						}
+    public static void scale(final View view, float fromScale, float toScale, long duration, final Runnable whenDone) {
+        if (Build.VERSION.SDK_INT >= 12) {
+            if (duration == 0) {
+                view.setScaleX(toScale);
+                view.setScaleY(toScale);
+                if (whenDone != null)
+                    whenDone.run();
+            } else {
+                ViewPropertyAnimator animation = view.animate().scaleX(toScale).scaleY(toScale).setDuration(duration);
+                if (whenDone != null) {
+                    animation.setListener(new AnimatorListener() {
+                        @Override
+                        public void onAnimationCancel(Animator animation) {
+                            whenDone.run();
+                        }
 
-						@Override
-						public void onAnimationEnd(Animator animation)
-						{
-							whenDone.run();
-						}
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            whenDone.run();
+                        }
 
-						@Override
-						public void onAnimationRepeat(Animator animation)
-						{
-						}
+                        @Override
+                        public void onAnimationRepeat(Animator animation) {
+                        }
 
-						@Override
-						public void onAnimationStart(Animator animation)
-						{
-						}
+                        @Override
+                        public void onAnimationStart(Animator animation) {
+                        }
 
-					});
-				}
-				animation.start();
-			}
-		}
-		else
-		{
-			ScaleAnimation scale = new ScaleAnimation(fromScale, toScale, fromScale, toScale, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
-					0.5f);
-			scale.setDuration(duration);
-			scale.setFillEnabled(true);
-			scale.setFillBefore(true);
-			scale.setFillAfter(true);
+                    });
+                }
+                animation.start();
+            }
+        } else {
+            ScaleAnimation scale = new ScaleAnimation(fromScale, toScale, fromScale, toScale, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
+                    0.5f);
+            scale.setDuration(duration);
+            scale.setFillEnabled(true);
+            scale.setFillBefore(true);
+            scale.setFillAfter(true);
 
-			if (whenDone != null)
-			{
-				scale.setAnimationListener(new AnimationListener()
-				{
-					@Override
-					public void onAnimationEnd(Animation animation)
-					{
-						whenDone.run();
-					}
+            if (whenDone != null) {
+                scale.setAnimationListener(new AnimationListener() {
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        whenDone.run();
+                    }
 
-					@Override
-					public void onAnimationRepeat(Animation animation)
-					{
-					}
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+                    }
 
-					@Override
-					public void onAnimationStart(Animation animation)
-					{
-					}
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+                    }
 
-				});
-			}
-			addAnimation(view, scale);
-		}
-	}
+                });
+            }
+            addAnimation(view, scale);
+        }
+    }
 
-	static void addAnimation(View view, Animation animation)
-	{
-		addAnimation(view, animation, false);
-	}
+    static void addAnimation(View view, Animation animation) {
+        addAnimation(view, animation, false);
+    }
 
-	static void addAnimation(View view, Animation animation, boolean first)
-	{
-		Animation previousAnimation = view.getAnimation();
-		if (previousAnimation == null || previousAnimation.getClass() == animation.getClass())
-		{
-			if (animation.getStartTime() == Animation.START_ON_FIRST_FRAME)
-				view.startAnimation(animation);
-			else
-				view.setAnimation(animation);
-			return;
-		}
+    static void addAnimation(View view, Animation animation, boolean first) {
+        Animation previousAnimation = view.getAnimation();
+        if (previousAnimation == null || previousAnimation.getClass() == animation.getClass()) {
+            if (animation.getStartTime() == Animation.START_ON_FIRST_FRAME)
+                view.startAnimation(animation);
+            else
+                view.setAnimation(animation);
+            return;
+        }
 
-		if (!(previousAnimation instanceof AnimationSet))
-		{
-			AnimationSet newSet = new AnimationSet(false);
-			newSet.addAnimation(previousAnimation);
-			previousAnimation = newSet;
-		}
+        if (!(previousAnimation instanceof AnimationSet)) {
+            AnimationSet newSet = new AnimationSet(false);
+            newSet.addAnimation(previousAnimation);
+            previousAnimation = newSet;
+        }
 
-		// Remove old of same type
-		//
-		AnimationSet set = (AnimationSet) previousAnimation;
-		for (int i = 0; i < set.getAnimations().size(); i++)
-		{
-			Animation anim = set.getAnimations().get(i);
-			if (anim.getClass() == animation.getClass())
-			{
-				set.getAnimations().remove(i);
-				break;
-			}
-		}
+        // Remove old of same type
+        //
+        AnimationSet set = (AnimationSet) previousAnimation;
+        for (int i = 0; i < set.getAnimations().size(); i++) {
+            Animation anim = set.getAnimations().get(i);
+            if (anim.getClass() == animation.getClass()) {
+                set.getAnimations().remove(i);
+                break;
+            }
+        }
 
-		// Add this (first if it is a scale animation ,else at end)
-		if (animation instanceof ScaleAnimation || first)
-		{
-			set.getAnimations().add(0, animation);
-		}
-		else
-		{
-			set.getAnimations().add(animation);
-		}
+        // Add this (first if it is a scale animation ,else at end)
+        if (animation instanceof ScaleAnimation || first) {
+            set.getAnimations().add(0, animation);
+        } else {
+            set.getAnimations().add(animation);
+        }
 
-		animation.startNow();
-		view.setAnimation(set);
-	}
+        animation.startNow();
+        view.setAnimation(set);
+    }
 }
