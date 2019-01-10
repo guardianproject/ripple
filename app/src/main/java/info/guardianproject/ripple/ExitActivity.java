ed412828e977d2a56021d3f0f0e1d2abd39c457c
Hans-Christoph Steiner <hans@eds.org>
2019-01-10 23:44:00 +0100

    fix InlinedApi: Using inlined constants on older versions

--- app/src/main/java/info/guardianproject/ripple/ExitActivity.java
+++ app/src/main/java/info/guardianproject/ripple/ExitActivity.java
@@ -1,8 +1,8 @@
 
 package info.guardianproject.ripple;
 
-import android.content.Intent;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 
@@ -24,10 +24,16 @@ public class ExitActivity extends Activity {
     public static void exitAndRemoveFromRecentApps(Activity activity) {
         Intent intent = new Intent(activity, ExitActivity.class);
 
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
-                        | Intent.FLAG_ACTIVITY_CLEAR_TASK
-                        | Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS
-                        | Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        if (Build.VERSION.SDK_INT < 11) {
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                    | Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS
+                    | Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        } else {
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                    | Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    | Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS
+                    | Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        }
 
         activity.startActivity(intent);
     }
